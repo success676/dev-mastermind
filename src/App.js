88e7c8e2f7ddb3f32c458e324a85be83db5f6dc9
success676 <matvey.filatov.03@mail.ru>
2025-04-21 20:03:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/main.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
@@ -22,6 +22,8 @@ function App() {
           <Route path="/skills" element={<Skills />} />
           <Route path="/project/:id" element={<Project />} />
           <Route path="/contacts" element={<Contacts />} />
+
+          <Route path="/" element={<Navigate to="/dev-mastermind" />} />
         </Routes>
         <Footer />
       </Router>
